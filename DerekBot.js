925d3c1385fdaff20a062e7bdb7a3022e8f24ed2
DerekLow18 <dl9525@bard.edu>
2016-08-08 13:07:42 -0400

    Update DerekBot.js

--- DerekBot.js
+++ DerekBot.js
@@ -240,7 +240,7 @@ function interact() {
   var t2 = new Date();
   t2.setSeconds(t2.getSeconds() + 2);
   r.innerHTML = " USER &nbsp;[" + t + "]: " + q.value + "<br>" + r.innerHTML ;
-  r.innerHTML = " ELIZA [" + t2 + "]: <code>" + response + "</code><br>" + r.innerHTML ;
+  r.innerHTML = " Derek [" + t2 + "]: <code>" + response + "</code><br>" + r.innerHTML ;
   q.value = "";
 }
 
